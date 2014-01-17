068a947e125012d09a1fd57b3c0785c32e39e21f
bitgeeky <mozpankaj1994@gmail.com>
2014-01-17 11:45:58 +0530

    added 3 frames

--- new.py
+++ new.py
@@ -6,6 +6,9 @@ import os
 
 class WaveOptions(Frame):
   
+    ampval = 1.00
+    tshiftval = 1.00
+    trevval = 1.00
     def __init__(self, parent):
         Frame.__init__(self, parent)   
          
@@ -15,27 +18,28 @@ class WaveOptions(Frame):
         
     def initUI(self):
       
-        self.parent.title("Wave Mixer")
+     #   self.parent.title("Wave Mixer")
         selectbutton = Button(self.parent, text="Select", command = self.ShowBrowser)
         selectbutton.pack(anchor=CENTER)
     
         ##work for amp slider
         var = DoubleVar()
-        scale = Scale( self.parent, variable = var, orient=HORIZONTAL )
+        scale = Scale( self.parent, variable = self.ampval, orient=HORIZONTAL )
         scale.pack(anchor=CENTER)
         alabel = Label(self.parent,text = "Amplitude")
         alabel.pack()
+        print self.ampval
 
         ##work for Time Shift slider
         var = DoubleVar()
-        scale = Scale( self.parent, variable = var, orient=HORIZONTAL )
+        scale = Scale( self.parent, variable = self.tshiftval, orient=HORIZONTAL )
         scale.pack(anchor=CENTER)
         alabel = Label(self.parent,text = "Time Shift")
         alabel.pack()
 
         ##work for Time Scaling slider
         var = DoubleVar()
-        scale = Scale( self.parent, variable = var, orient=HORIZONTAL )
+        scale = Scale( self.parent, variable = self.trevval, orient=HORIZONTAL )
         scale.pack(anchor=CENTER)
         alabel = Label(self.parent,text = "Time Scaling")
         alabel.pack()
@@ -67,8 +71,16 @@ class WaveOptions(Frame):
 def main():
   
     root = Tk()
-    root.geometry("300x400+300+300")
-    app = WaveOptions(root)
+    root.geometry("700x400+300+300")
+    frame_a = LabelFrame(root, text='First frame', padx=5, pady=5)
+    frame_a.grid(sticky=E,row = 0, column = 0)
+    app = WaveOptions(frame_a)
+    frame_b = LabelFrame(root, text='Second frame', padx=5, pady=5)
+    frame_b.grid(sticky=W, row = 0, column = 1)
+    app2 = WaveOptions(frame_b)
+    frame_c = LabelFrame(root, text='Third frame', padx=5, pady=5)
+    frame_c.grid(sticky=W, row = 0, column = 2)
+    app3 = WaveOptions(frame_c)
     root.mainloop()  
 
 
