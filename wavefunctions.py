818da5e73019cabf8106311318ebca7cca52b9ff
bitgeeky <mozpankaj1994@gmail.com>
2014-01-21 11:56:42 +0530

    added scaling and shifting

--- wavefunctions.py
+++ wavefunctions.py
@@ -35,11 +35,46 @@ class WaveFunctions:
                 self.new_data[i] = -32768
             else:
                 self.new_data[i]=self.new_data[i]*qty
-    """
+    
     def shift(self, factor):
+        ftoshift = factor * self.samplerate
+        ftoshift = abs(ftoshift)
+        if fact < 0:
+            if self.channel == 1:
+                self.new_data = self.new_data[ftoshift::1]
+            else:
+                self.new_data = self.new_data[2*ftoshift::1]
+        
+        else:
+            if self.channel == 1:
+                for i in range(ftoshift):
+                    new.append(0)
+            else:
+                for i in range(2*ftoshift):
+                    new.append(0)
+            self.new_data = new + self.new_data
+
+        self.frames = len(self.new_data)/self.channel
     
     def scale(self, factor):
-    """
+        new = []
+        new1 = []
+        new2 = []
+        if self.channel = 1:
+            for i in range(int(len(self.new_data)/factor)):
+                new.append(self.new_data[int(factor*i)])
+        else:
+            for i in range(len(self.new_data)):
+                if i%2 == 0:
+                    new2.append(self.new_data[i])
+                else:
+                    new1.append(self.new_data[i])
+            for i in range(int(len(new2)/factor)):
+                new.append(new2[int(factor*i)])
+                new.append(new1[int(factor*i)])
+        self.new_data = new
+        self.frames = len(self.new_data)/self.channel
+    
     def write(self, name):
         if self.samplewidth==1:
             fmt="%iB" %self.frames*self.channel
